d73eb1e1e9feeed7a70c7cec350dbde7b613a7cf
Newa <sth.shiva34@gmail.com>
2015-01-25 17:52:55 +0200

    Average rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings
+	
+ def average_rating
+	if self.ratings.count > 0
+		return ratings.inject(0) {|sum, n | sum + n.score } / self.ratings.count
+	else
+		return 0;
+	end
+ end
+		
 end
