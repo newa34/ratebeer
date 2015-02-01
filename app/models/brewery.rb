cb1befb3015fd3576eb5dd2b21173be00aeeb4e8
Newa <sth.shiva34@gmail.com>
2015-02-01 12:07:01 +0200

    task 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,5 +3,14 @@ class Brewery < ActiveRecord::Base
 	has_many :ratings, through: :beers
 	
 	include RatingAverage	
-	validates :year, numericality: {only_integer: true, greater_than_or_equal_to: 1042, less_than_or_equal_to: 2015}
+	validates :year, numericality: {only_integer: true, greater_than_or_equal_to: 1042}
+	validate :year_current
+
+	def year_current
+		if year > Time.now.year
+			errors.add(:year, "Year cannot be Future ")
+		end
+	end
+
 end
+
