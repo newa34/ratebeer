7df3ca5189bfdc9e1bbea86608eba27635c3d6af
Newa <sth.shiva34@gmail.com>
2015-02-01 17:27:24 +0200

    Validation for Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
 	include RatingAverage
 	
-	validates :username, uniqueness: true, length: {minimum: 3, maximum: 15}
+	has_secure_password
 
-	has_many :ratings
 	has_many :beers, through: :ratings
 	has_many :memberships, dependent: :destroy
 	has_many :beer_clubs, through: :memberships
-	
+	has_many :ratings
+
+	validates :username, uniqueness: true, length: {minimum: 3, maximum: 15}
+	validates :password_confirmation, presence: true
+	validates :password, format: {with: /(?=.*\d)/, message: "Atleast one number"}
+	validates :password, format: {with: /(?=.*[A-Z])/, message: "Atleast one uppercase"}
+
 end
