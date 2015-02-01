ae92085138de8d0a8227144e8c46df3577823062
Newa <sth.shiva34@gmail.com>
2015-02-01 19:23:32 +0200

    dependency - Task 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	has_many :beers, through: :ratings
-	has_many :memberships, dependent: :destroy
 	has_many :beer_clubs, through: :memberships
-	has_many :ratings
+	has_many :ratings, depedent: :destroy
+	has_many :memberships, dependent: :destroy
 
 	validates :username, uniqueness: true, length: {minimum: 3, maximum: 15}
 	validates :password_confirmation, presence: true
+	validates :username, length: {minimum: 4}
 	validates :password, format: {with: /(?=.*\d)/, message: "Atleast one number"}
 	validates :password, format: {with: /(?=.*[A-Z])/, message: "Atleast one uppercase"}
 
