2b449d5a5471dc95e162a08a352a6487174db43a
Newa <sth.shiva34@gmail.com>
2015-02-15 21:33:19 +0200

    Week 5 Task 1

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Ratebeer
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-#    config.autoload_paths += Dir["# {Rails.root}/lib"]
+    config.autoload_paths += Dir["# {Rails.root}/lib"]
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
