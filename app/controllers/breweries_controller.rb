992a8764da3e3170abd6e096d0aabbc0e38a242a
Newa <sth.shiva34@gmail.com>
2015-03-06 14:47:20 +0200

    modified User Controller

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,6 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_that_signed_in, except: [:index, :show, :brewerylist]
+  before_action :ensure_that_admin, except: [:index, :show, :brewerylist]
 
   def brewerylist
   end
